b15c5e67ba1565a413a7c3d20f47f0c68c3871eb
sonshine <stevenjetton@gmail.com>
2018-03-09 08:51:47 -0600

    added blog to home page

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -13,7 +13,6 @@
  */
 
 get_header(); ?>
-<pre><?php print_r($wp_query); exit; ?></pre>
 	<div id="primary" class="home-page hero-content">
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
@@ -22,5 +21,18 @@ get_header(); ?>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
+	<section class="recent-posts">
+		<div class="site-content">
+			<div class="blog-post">
+				<h4>From the Blog</h4>
+				<?php query_posts('posts_per_page=1'); ?>
+				<?php while (have_posts() ) : the_post(); ?>
+					<h2><?php the_title(); ?></h2>
+					<?php the_excerpt(); ?>
+				<?php endwhile; ?>
+				<?php wp_reset_query(); ?>
+			</div>
+		</div>
+	</section>
 
 <?php get_footer(); ?>
